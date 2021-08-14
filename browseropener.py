1caf0909d7cfd66ebefd4298e0c4e9cc43c4a6b9
HFH-neocity <70291177+HFH-neocity@users.noreply.github.com>
2021-08-14 11:49:48 -0700

    Update browseropener.py

--- browseropener.py
+++ browseropener.py
@@ -5,8 +5,16 @@ import time
 driver = webdriver.Firefox(
     executable_path=r'D:\coding projects\geckodriver.exe')
 
-driver.get("https://lainchan.org/%CE%94/catalog.html")
-
+p = input("enter a board you wanna go to:")
+if p == "tech":
+    driver.get("https://lainchan.org/%CE%A9/catalog.html")
+elif p == "programming":
+    driver.get("https://lainchan.org/%CE%BB/catalog.html")
+elif p == "security":
+    driver.get("https://lainchan.org/sec/catalog.html")
+elif p == "diy":
+    driver.get("https://lainchan.org/%CE%94/catalog.html")
+    
 try:
     results = driver.find_element_by_class_name("threads")
     print(results.text)
